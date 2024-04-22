817dd2e05f66ebe968aeb1eccdb3d6a4f88d625d
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-04-22 14:47:12 +0200

    adds hero image to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import backgroundBeach from "@/public/background-beach.jpg";
+
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col">
-			<h1>Welcome to Holidaze</h1>
+		<main className="flex flex-col m-auto min-h-screen flex-col max-w-7xl">
+			<div
+				style={{
+					backgroundImage: `url(${backgroundBeach.src})`,
+					backgroundSize: "cover",
+				}}
+				className="flex items-center py-40"
+			>
+				<h1>Holidaze</h1>
+			</div>
 		</main>
 	);
 }
