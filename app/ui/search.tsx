bceaa50d5ebd336c34b186858b2104902749c1a1
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-04-23 14:34:26 +0200

    adds basic functionality to search input as well as changes to its styles

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,13 +1,36 @@
 "use client";
 
-export default function Search() {
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+
+const Search = () => {
+	const searchParams = useSearchParams();
+	const pathname = usePathname();
+	const { replace } = useRouter();
+
+	const handleSearch = (searchTerm: string) => {
+		const params = new URLSearchParams(searchParams);
+		params.set("page", "1");
+		if (searchTerm) {
+			params.set("query", searchTerm);
+		} else {
+			params.delete("query");
+		}
+		replace(`${pathname}?${params.toString()}`);
+	};
+
 	return (
 		<div className="flex flex-col text-blue w-full">
 			<label htmlFor="search">Search:</label>
 			<input
 				className="rounded-full border border-yellow border-2 pl-4 py-2 placeholder:text-gray-500"
 				placeholder="Search venues..."
+				onChange={(e) => {
+					handleSearch(e.target.value);
+				}}
+				defaultValue={searchParams.get("query")?.toString()}
 			/>
 		</div>
 	);
-}
+};
+
+export default Search;
