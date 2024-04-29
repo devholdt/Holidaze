fd93ac65d72e97c390a47311bbdf80fda6f3f687
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-04-29 12:50:33 +0200

    removes CreateCard component and moves relevant code to VenueList component

--- app/ui/venues/venue-list.tsx
+++ app/ui/venues/venue-list.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import { Venue } from "@/app/lib/definitions";
-import { FC, useState } from "react";
-import { CreateCard } from "./create-card";
 import Button from "@/app/ui/button";
+import Image from "next/image";
+import { FC, useState } from "react";
+import { Venue } from "@/app/lib/definitions";
+import { elMessiri } from "@/app/ui/fonts";
+import { formatNumber } from "@/app/lib/utils";
+import { motion } from "framer-motion";
 
 const VenueList: FC<{
 	venues: Venue[];
@@ -17,7 +20,34 @@ const VenueList: FC<{
 		<>
 			<div className="grid grid-cols-3 gap-8 w-full px-4 md:px-14 xl:px-32">
 				{venues.slice(0, limit).map((venue) => (
-					<CreateCard key={venue.id} venue={venue} />
+					<motion.div
+						key={venue.id}
+						className="relative text-white flex flex-col justify-end h-96 p-2 hover:cursor-pointer"
+						whileHover={{ scale: 1.05 }}
+						whileTap={{ scale: 1.02 }}
+					>
+						<Image
+							src={venue.media[0]?.url}
+							alt={venue.media[0]?.alt}
+							onError={(event) => {
+								const target = event.target as HTMLImageElement;
+								target.src = "./background-reflection.jpg";
+							}}
+							fill
+							unoptimized
+							className="object-cover object-center z-1 drop-shadow-md"
+						/>
+
+						<div className="flex flex-col gap-1 z-10 px-4 py-2 bg-darkBrown border border-brown">
+							<p className="font-light">
+								<span className="break-all">£{formatNumber(venue.price)}</span>{" "}
+								<span className="uppercase">/ night</span>
+							</p>
+							<h4 className={`${elMessiri.className} truncate`}>
+								{venue.name}
+							</h4>
+						</div>
+					</motion.div>
 				))}
 			</div>
 			{showMoreButton && venues && limit < venues.length && (
