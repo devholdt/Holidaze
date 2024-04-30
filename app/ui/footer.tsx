8028f0036d411f7eb58adec71d3c81a4c8ff01ac
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-04-30 12:17:08 +0200

    edits prettierignore and prettier config

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -2,22 +2,22 @@ import Logo from "@/app/ui/holidaze-logo";
 import logoWhite from "@/public/logo-white.svg";
 
 export default function Footer() {
-	return (
-		<footer className="flex flex-col bg-darkBrown">
-			<div className="flex justify-around items-center p-12">
-				<div>
-					<Logo src={logoWhite} width={200} height={60} />
-				</div>
-				<div>
-					<p className="text-lightBrown max-w-96">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua.
-					</p>
-				</div>
-			</div>
-			<div className="flex justify-center text-lightBrown bg-darkerBrown w-full p-4">
-				© Holidaze Resorts 2024
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="flex flex-col bg-darkBrown">
+      <div className="flex justify-around items-center p-12">
+        <div>
+          <Logo src={logoWhite} width={200} height={60} />
+        </div>
+        <div>
+          <p className="text-lightBrown max-w-96">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-center text-lightBrown bg-darkerBrown w-full p-4">
+        © Holidaze Resorts 2024
+      </div>
+    </footer>
+  );
 }
--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -5,30 +5,30 @@ import UserDropdown from "@/app/ui/user/user-dropdown";
 import waterImg from "@/public/texture-water-sm.jpg";
 
 export default function Header() {
-	return (
-		<header
-			style={{
-				backgroundImage: `url(${waterImg.src})`,
-				backgroundSize: "cover",
-			}}
-		>
-			<div className="flex justify-between items-center py-8 px-10 max-w-7xl m-auto">
-				<Link href="/">
-					<Logo src={logoWhite} width={160} height={80} />
-				</Link>
-				<nav className="flex gap-8 items-center">
-					<Link href="/" className="text-white uppercase">
-						Home
-					</Link>
-					<Link href="/venues" className="text-white uppercase">
-						Venues
-					</Link>
-					<Link href="/contact" className="text-white uppercase">
-						Contact
-					</Link>
-					<UserDropdown />
-				</nav>
-			</div>
-		</header>
-	);
+  return (
+    <header
+      style={{
+        backgroundImage: `url(${waterImg.src})`,
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="flex justify-between items-center py-8 px-10 max-w-7xl m-auto">
+        <Link href="/">
+          <Logo src={logoWhite} width={160} height={80} />
+        </Link>
+        <nav className="flex gap-8 items-center">
+          <Link href="/" className="text-white uppercase">
+            Home
+          </Link>
+          <Link href="/venues" className="text-white uppercase">
+            Venues
+          </Link>
+          <Link href="/contact" className="text-white uppercase">
+            Contact
+          </Link>
+          <UserDropdown />
+        </nav>
+      </div>
+    </header>
+  );
 }
