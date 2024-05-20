b76f6357b31007d9da061a52fc826afc1d9f9576
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-05-20 12:03:20 +0200

    slightly modifies styling on home page

--- app/page.tsx
+++ app/page.tsx
@@ -15,45 +15,46 @@ const VenueList = dynamic(() => import("@/app/ui/venues/VenueList"), {
 
 const Page: React.FC = () => {
    return (
-      <main className="m-auto flex min-h-screen max-w-7xl flex-col border-x border-lightGrey bg-background">
+      <main className="m-auto flex min-h-screen max-w-7xl flex-col bg-background">
          <div
             style={{
                backgroundImage: `url(${waterImg.src})`,
                backgroundSize: "cover",
             }}
-            className="flex h-96 items-center justify-center"
+            className="flex h-[240px] items-center justify-center xs:h-[320px] md:h-[400px]"
          >
             <h1>
-               <Logo src={logoWhiteSubtitle} styles="w-[480px] px-4" />
+               <Logo src={logoWhiteSubtitle} styles="w-[480px] px-8" />
             </h1>
          </div>
 
-         <Hero
-            heading="Serene Escapes"
-            headingLevel={2}
-            subheading="Welcome"
-            text="Explore our global collection of exquisite resorts, each
+         <div className="border-x border-lightGrey">
+            <Hero
+               heading="Serene Escapes"
+               headingLevel={2}
+               subheading="Welcome"
+               text="Explore our global collection of exquisite resorts, each
                   offering a unique sanctuary of peace and luxury. Dive into
                   unparalleled comfort and serene settings, where every stay
                   promises a memorable escape."
-            styles="hidden sm:flex"
-         />
-
-         <div className="mt-36 flex flex-col items-center">
-            <div className="mb-12 flex flex-col items-center text-center">
-               <h3 className={`${elMessiri.className} leading-tight`}>
-                  Bookings & Venues
-               </h3>
-               <Subheading
-                  text={"Dream Vacations"}
-                  left={"w-24 me-2"}
-                  right={"w-24 ms-2"}
-               />
+               styles="hidden sm:flex"
+            />
+            <div className="mt-36 flex flex-col items-center">
+               <div className="mb-12 flex flex-col items-center px-4 text-center">
+                  <h3 className={`${elMessiri.className} leading-tight`}>
+                     Bookings & Venues
+                  </h3>
+                  <Subheading
+                     text={"Dream Vacations"}
+                     left={"w-10 xs:w-24 me-2"}
+                     right={"w-10 xs:w-24 ms-2"}
+                  />
+               </div>
             </div>
-         </div>
-         <div className="mb-36 flex flex-col items-center">
-            <VenueList listLimit={4} />
-            <LinkButton targetHref="/venues" text="View all" styles="mt-8" />
+            <div className="mb-36 flex flex-col items-center">
+               <VenueList listLimit={4} />
+               <LinkButton targetHref="/venues" text="View all" styles="mt-8" />
+            </div>
          </div>
       </main>
    );
