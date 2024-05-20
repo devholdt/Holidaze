b8f2b83352c375b47da765ae0f4a4f87e68658f0
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-05-20 11:56:27 +0200

    removes redundant content from footer component

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,22 +1,11 @@
-import Logo from "@/app/ui/Logo";
-import logoWhite from "@/public/logo-white.svg";
-
 export default function Footer() {
+   const date = new Date();
+   const year = date.getFullYear();
+
    return (
       <footer className="flex flex-col bg-darkBrown">
-         <div className="flex items-center justify-around p-12">
-            <div>
-               <Logo src={logoWhite} styles="max-w-[200px]" />
-            </div>
-            <div>
-               <p className="max-w-96 text-lightBrown">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-               </p>
-            </div>
-         </div>
          <div className="flex w-full justify-center bg-darkerBrown p-4 text-lightBrown">
-            © Holidaze Resorts 2024
+            © Holidaze Resorts {year}
          </div>
       </footer>
    );
