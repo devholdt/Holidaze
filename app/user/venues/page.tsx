a8b9ece69177672000612cde1ea0a9b035c7efab
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-05-26 13:40:54 +0200

    improves heading hierarchy further for all pages

--- app/user/venues/page.tsx
+++ app/user/venues/page.tsx
@@ -4,9 +4,6 @@ import { redirect } from "next/navigation";
 import Hero from "@/app/ui/Hero";
 import ManagerVenueList from "@/app/ui/user/venues/ManagerVenueList";
 import Breadcrumbs from "@/app/ui/Breadcrumbs";
-// import dynamic from "next/dynamic";
-
-// const Modal = dynamic(() => import("@/app/ui/Modal"));
 
 export const metadata: Metadata = {
    title: "Your Venues",
@@ -38,17 +35,6 @@ export default function Page() {
             ]}
          />
          <Hero heading="Venues" headingLevel={1} subheading="Your" />
-
-         {/* <div className="mb-4 mt-8 flex w-full flex-col items-center justify-center gap-2 bg-white py-4 shadow">
-            <h2 className="text-center text-lg font-light uppercase tracking-wider text-blue sm:text-xl">
-               Be the curator of someones next adventure
-            </h2>
-            <Modal
-               modal="Create venue"
-               textContent="Create venue"
-               buttonStyles="px-8 py-3 w-max text-lg font-extralight uppercase tracking-widest transition bg-brown text-white hover:bg-darkBrown"
-            />
-         </div> */}
 
          <ManagerVenueList name={nameValue ?? ""} />
       </main>
