bfadcd74df63ebb3531888186822d231fa9cf3dc
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-05-26 13:45:46 +0200

    improves Hero component to utilize nextjs's Image component for better optimization

--- app/ui/hero.tsx
+++ app/ui/hero.tsx
@@ -1,6 +1,7 @@
 import { createElement } from "react";
 import { elMessiri } from "@/app/ui/fonts";
 import { HeroProps } from "@/app/lib/definitions";
+import Image from "next/image";
 import Subheading from "@/app/ui/Subheading";
 import backgroundImage from "@/public/background-beach.jpg";
 
@@ -21,14 +22,16 @@ const Hero: React.FC<HeroProps> = ({
 
    return (
       <div
-         style={{
-            backgroundImage: `url(${backgroundImage.src})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-         }}
-         className={`flex h-64 flex-col justify-center border-y border-white p-0 text-dark xs:p-12 md:h-80 md:p-28 ${styles}`}
+         className={`relative z-10 flex h-64 flex-col justify-center border-y border-white p-0 text-dark xs:p-12 md:h-80 md:p-28 ${styles}`}
       >
-         <div className="flex flex-col items-center xs:items-start">
+         <Image
+            src={backgroundImage.src}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+            alt="beach with waves crashing on shore from a bird eye view"
+         />
+         <div className="z-20 flex flex-col items-center xs:items-start">
             <div>
                {subheading && <Subheading text={subheading} left={""} />}
                <div className="text-6xl">{Heading}</div>
